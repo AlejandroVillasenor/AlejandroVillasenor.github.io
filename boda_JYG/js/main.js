553bc22670b7ba78a4419a7f154dc32f397aebfe
AlejandroVillasenor <92410341+AlejandroVillasenor@users.noreply.github.com>
2022-04-16 14:54:41 -0500

    Add files via upload

---
+++ boda_JYG/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    //nav toggle
+    $(".hamburger-btn").click(function() {
+        $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+        if($(window).width() < 768){
+            $(".header .nav").slideToggle();
+        }
+    })
+    //header
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.header').addClass('fixed');
+        } else {
+            $('.header').removeClass('fixed');
+        }
+    })
+    //scroll
+    $.scrollIt({
+        topOffset: -50,
+    });
+    //popup de las fotos
+    const wHeight = $(window).height();
+    $(".gallery-popup .gp-img").css("max-height", wHeight + "px");
+
+    let itemIndex = 0;
+    const totalGalleryItems = $(".gallery-item").length;
+    $(".gallery-item").click(function() {
+        itemIndex = $(this).index();
+        $(".gallery-popup").addClass("open");
+        $(".gallery-popup .gp-img").hide();
+        gpSlideShow();
+    })
+    $(".gp-controls .next").click(function() {
+        if(itemIndex == totalGalleryItems - 1) {
+            itemIndex = 0;
+        } else {
+            itemIndex++;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function() {
+            gpSlideShow();
+        });
+    })
+    $(".gp-controls .prev").click(function() {
+        if(itemIndex === 0) {
+            itemIndex = totalGalleryItems - 1;
+        } else {
+            itemIndex--;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function() {
+            gpSlideShow();
+        });
+    })
+
+    function gpSlideShow() {
+        const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+        $(".gallery-popup .gp-img").fadeIn().attr("src", imgSrc);
+        $(".gp-counter").text(itemIndex + 1 + "/" + totalGalleryItems);
+    }
+    //Fin del popup de las fotos
+    $(".gp-close").click(function() {
+        $(".gallery-popup").removeClass("open");
+    })
+    $(".gallery-popup").click(function(event) { //Al hacer click fuera del popup
+        if($(event.target).hasClass("open")) {
+            $(".gallery-popup").removeClass("open");
+        }
+    })
+});
